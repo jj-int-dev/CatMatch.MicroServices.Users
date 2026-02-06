31a6cd2f6a56dea851dfac84c533e6160bfb3e92
jj-int-dev <jjollpsl@gmail.com>
2026-02-06 13:20:07 -0500

    add binding for render

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import config from './config/config';
 
-app.listen(config.PORT, () => {
+app.listen(config.PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${config.PORT}`);
 });
